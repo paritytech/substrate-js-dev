3a75dd89d6176c08039b779eeeb00bfea21f3e3f
emostov <32168567+emostov@users.noreply.github.com>
2021-03-09 18:25:02 -0800

    Update lint setting

--- config/prettier.js
+++ config/prettier.js
@@ -4,4 +4,3 @@ module.exports = {
 	tabWidth: 2,
 	useTabs: true,
 };
-
--- eslintrc.js
+++ eslintrc.js
@@ -3,8 +3,7 @@ const base = require('./config/eslint.js');
 module.exports = {
 	...base,
 	ignorePatterns: [
-		'.eslintrc.js',
 		'**/node_modules/*'
 	],
 	parserOptions: {}
-}
+};
