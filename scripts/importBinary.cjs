4cf5b73238cdbd76bcdf6beed1ed53e292b9d926
tarikgul <tarik@parity.io>
2022-05-02 18:46:52 -0400

    fix binary

--- scripts/importBinary.cjs
+++ scripts/importBinary.cjs
@@ -1,6 +1,6 @@
 const path = require('path');
 
-module.exports  = function importBinary (bin, dir) {
+module.exports = function importBinary (bin, dir) {
     console.log(`${bin} ${process.argv.slice(2).join(' ')}`);
 
     return require(path.join(process.cwd(), 'node_modules', dir));
