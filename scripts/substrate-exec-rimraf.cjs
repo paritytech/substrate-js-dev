5784ceac9fe7541e22f4fdcab3a3965fe70a99db
Tarik Gul <47201679+TarikGul@users.noreply.github.com>
2024-10-23 11:05:01 -0700

    fix: write a custom rimraf (#129)

--- scripts/substrate-exec-rimraf.cjs
+++ scripts/substrate-exec-rimraf.cjs
@@ -1,6 +1,21 @@
 #!/usr/bin/env node
-// Copyright 2022 Parity Technologies (UK) Ltd.
+// Copyright 2024 Parity Technologies (UK) Ltd.
 
-const importBinary = require('./importBinary.cjs');
+const fs = require('fs');
 
-importBinary('rimraf', 'rimraf/bin');
+(function() {
+    const dirs = process.argv.slice(2);
+
+    if (!dirs.length) {
+        throw new Error(`rimraf-exec: Invalid args length: retrieved ${dirs.length}`)
+    }
+    
+    for (const dir of dirs) {
+        if (fs.existsSync(dir)) {
+          console.log(`rimraf-exec: ${dir}`)
+          fs.rmSync(dir, { force: true, recursive: true });
+        } else {
+            console.warn(`rimraf-exec: ${dir} is an invalid file path and does not exist`)
+        }
+    }
+})();
