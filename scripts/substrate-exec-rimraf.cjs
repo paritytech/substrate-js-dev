87ca1cfaf2c857036ed8f1f4f966b2ca64cfef1f
tarikgul <tariksnow37@gmail.com>
2024-10-23 11:00:21 -0400

    Write a custom rimraf

--- scripts/substrate-exec-rimraf.cjs
+++ scripts/substrate-exec-rimraf.cjs
@@ -1,6 +1,13 @@
 #!/usr/bin/env node
 // Copyright 2022 Parity Technologies (UK) Ltd.
+const fs = require('fs');
 
-const importBinary = require('./importBinary.cjs');
-
-importBinary('rimraf', 'rimraf/bin');
+(function() {
+    const dirs = process.argv.slice(2);
+    
+    for (const dir of dirs) {
+        if (fs.existsSync(dir)) {
+          fs.rmSync(dir, { force: true, recursive: true });
+        }
+    }
+})();
